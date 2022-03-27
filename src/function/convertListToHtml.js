e3f31eab721746c6f876b5425c6d344875778175
proceane <proceane1@gmail.com>
2022-03-27 18:43:37 +0900

    encoding logo name

--- src/function/convertListToHtml.js
+++ src/function/convertListToHtml.js
@@ -3,7 +3,7 @@ const methods = {
     let result = "";
     for (let key in skillList) {
       result += ("<img src=\"https://img.shields.io/badge/"
-      + skillList[key]['name']
+      + encodeURIComponent(skillList[key]['name'])
       + "-"
       + skillList[key]['color']
       + "?style=flat-square&logo="
