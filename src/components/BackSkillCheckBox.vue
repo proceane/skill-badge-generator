836bf82e87139bf8b926c1a15ff8cf1ecd22de85
proceane <proceane1@gmail.com>
2022-03-28 15:14:17 +0900

    add Back Skill CheckBox List

---
+++ src/components/BackSkillCheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-checkbox
+      v-for="item in backSkillList"
+      :key="item.skill"
+      :label="item.skill"
+      v-model="skillList"
+      v-bind:value="{'logo': item.logo, 'name': item.skill, 'color': item.color}"
+      hide-details
+      @change="onChange"
+    >
+    </v-checkbox>
+  </v-row>  
+</template>
+
+<script>
+
+import backSkillList from "../static/dev_skills_back.json";
+
+export default {
+  name: "BackSkillCheckBox",
+  data: () => ({
+    skillList: [],
+    backSkillList: backSkillList,
+  }),
+  methods: {
+    onChange() {
+      this.$emit('selectBackSkillList', this.skillList);
+    },
+  },
+}
+</script>
