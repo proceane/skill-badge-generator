0f9409701d610a8caa4908dfd9554acd942906c8
proceane <proceane1@gmail.com>
2022-03-28 13:53:12 +0900

    add LangSkillCheckBox.vue

---
+++ src/components/LangSkillCheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-checkbox
+      v-for="item in langSkillList"
+      :key="item.skill"
+      :label="item.skill"
+      v-model="skillList"
+      v-bind:value="{'logo': item.logo, 'name': item.skill, 'color': item.color}"
+      hide-details
+      @change="onChange"
+    >
+    </v-checkbox>
+  </v-row>  
+</template>
+
+<script>
+
+import langSkillList from "../static/dev_skills_lang.json";
+
+export default {
+  name: "LangSkillCheckBox",
+  data: () => ({
+    skillList: [],
+    langSkillList: langSkillList,
+  }),
+  methods: {
+    onChange() {
+      this.$emit('selectSkillList', this.skillList);
+    },
+  },
+}
+</script>
