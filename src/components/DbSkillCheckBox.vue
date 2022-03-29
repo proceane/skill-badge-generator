cb68e6c39a4de4037b3953c55551f1ede834c305
proceane <proceane1@gmail.com>
2022-03-29 23:07:04 +0900

    add DbSkillCheckBox

---
+++ src/components/DbSkillCheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-checkbox
+      v-for="item in dbSkillList"
+      :key="item.skill"
+      :label="item.skill"
+      v-model="skillList"
+      v-bind:value="{'logo': item.logo, 'name': item.skill, 'color': item.color}"
+      hide-details
+      @change="onChange"
+    >
+    </v-checkbox>
+  </v-row>  
+</template>
+
+<script>
+
+import dbSkillList from "../static/dev_skills_db.json";
+
+export default {
+  name: "DbSkillCheckBox",
+  data: () => ({
+    skillList: [],
+    dbSkillList: dbSkillList,
+  }),
+  methods: {
+    onChange() {
+      this.$emit('selectDbSkillList', this.skillList);
+    },
+  },
+}
+</script>
