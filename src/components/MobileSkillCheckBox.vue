888798dbc24597b1d56da9320d0ab5e9c33e8005
proceane <proceane1@gmail.com>
2022-03-30 20:59:22 +0900

    add MobileSkillCheckBox

---
+++ src/components/MobileSkillCheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-checkbox
+      v-for="item in mobileSkillList"
+      :key="item.skill"
+      :label="item.skill"
+      v-model="skillList"
+      v-bind:value="{'logo': item.logo, 'name': item.skill, 'color': item.color}"
+      hide-details
+      @change="onChange"
+    >
+    </v-checkbox>
+  </v-row>  
+</template>
+
+<script>
+
+import mobileSkillList from "../static/dev_skills_mobile.json";
+
+export default {
+  name: "MobileSkillCheckBox",
+  data: () => ({
+    skillList: [],
+    mobileSkillList: mobileSkillList,
+  }),
+  methods: {
+    onChange() {
+      this.$emit('selectMobileSkillList', this.skillList);
+    },
+  },
+}
+</script>
