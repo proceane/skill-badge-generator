c4d20abcacdf96720e7b7b19dc3c6e81dabb6bfb
proceane <proceane1@gmail.com>
2022-03-30 21:43:07 +0900

    add CollaboSkillCheckBox

---
+++ src/components/CollaboSkillCheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-checkbox
+      v-for="item in collaboSkillList"
+      :key="item.skill"
+      :label="item.skill"
+      v-model="skillList"
+      v-bind:value="{'logo': item.logo, 'name': item.skill, 'color': item.color}"
+      hide-details
+      @change="onChange"
+    >
+    </v-checkbox>
+  </v-row>  
+</template>
+
+<script>
+
+import collaboSkillList from "../static/dev_skills_collaborate.json";
+
+export default {
+  name: "CollaboSkillCheckBox",
+  data: () => ({
+    skillList: [],
+    collaboSkillList: collaboSkillList,
+  }),
+  methods: {
+    onChange() {
+      this.$emit('selectCollaboSkillList', this.skillList);
+    },
+  },
+}
+</script>
