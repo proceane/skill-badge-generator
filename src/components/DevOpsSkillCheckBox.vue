e0f041c40c12696bb0a47e11964542a5af4804f2
proceane <proceane1@gmail.com>
2022-03-30 21:22:48 +0900

    add DevOpsSkillCheckBox

---
+++ src/components/DevOpsSkillCheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-checkbox
+      v-for="item in devOpsSkillList"
+      :key="item.skill"
+      :label="item.skill"
+      v-model="skillList"
+      v-bind:value="{'logo': item.logo, 'name': item.skill, 'color': item.color}"
+      hide-details
+      @change="onChange"
+    >
+    </v-checkbox>
+  </v-row>  
+</template>
+
+<script>
+
+import devOpsSkillList from "../static/dev_skills_devops.json";
+
+export default {
+  name: "DevOpsSkillCheckBox",
+  data: () => ({
+    skillList: [],
+    devOpsSkillList: devOpsSkillList,
+  }),
+  methods: {
+    onChange() {
+      this.$emit('selectDevOpsSkillList', this.skillList);
+    },
+  },
+}
+</script>
