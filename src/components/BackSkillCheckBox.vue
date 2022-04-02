6d002d3b7847c37a89c151ec12dd4c02ded7258b
proceane <proceane1@gmail.com>
2022-04-02 23:01:33 +0900

    add input text area, search function

--- src/components/BackSkillCheckBox.vue
+++ src/components/BackSkillCheckBox.vue
@@ -1,15 +1,29 @@
 <template>
   <v-row>
-    <v-checkbox
-      v-for="item in backSkillList"
-      :key="item.skill"
-      :label="item.skill"
-      v-model="skillList"
-      v-bind:value="{'logo': item.logo, 'name': item.skill, 'color': item.color}"
-      hide-details
-      @change="onChange"
-    >
-    </v-checkbox>
+    <v-row>
+      <v-col
+        cols="4"
+      >
+        <v-text-field
+        label="search"
+        outlined
+        append-icon="mdi-magnify"
+        @input="onSearch"
+      ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-checkbox
+        v-for="item in backSkillList"
+        :key="item.skill"
+        :label="item.skill"
+        v-model="skillList"
+        v-bind:value="{'logo': item.logo, 'name': item.skill, 'color': item.color}"
+        hide-details
+        @change="onChange"
+      >
+      </v-checkbox>
+    </v-row>
   </v-row>  
 </template>
 
@@ -30,6 +44,14 @@ export default {
     executeResetCommand() {
       this.skillList = [];
     },
+    onSearch(value) {
+      if (value == '') {
+        this.backSkillList = backSkillList;
+      }
+      this.backSkillList = this.backSkillList.filter((e) => {
+        return e.skill.substr(0, value.length).toUpperCase() == value.toUpperCase();
+      });
+    }
   },
 }
 </script>
