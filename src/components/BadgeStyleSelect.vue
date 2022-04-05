9f62c0a66b4ba45be49c693b4b9718a92430ecde
proceane <proceane1@gmail.com>
2022-04-05 23:45:06 +0900

    add BadgeStyleSelect

---
+++ src/components/BadgeStyleSelect.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-select
+    label="Badge Style"
+    :items="badgeStyleList"
+  ></v-select>
+</template>
+
+<script>
+export default {
+  name: "BadgeStyleSelect",
+  data: () => ({
+    badgeStyleList: ['plastic', 'flat', 'flat-square', 'for-the-badge', 'social'],
+  }),
+}
+</script>
