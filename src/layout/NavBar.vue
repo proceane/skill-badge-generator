23b72394e12df6f82730c437f4922963ea43eb19
proceane <proceane1@gmail.com>
2022-04-13 19:16:27 +0900

    add hide-on-scroll in NavBar

--- src/layout/NavBar.vue
+++ src/layout/NavBar.vue
@@ -1,15 +1,16 @@
 <template>
   <v-app-bar
-      app
-      color="dark"
-      dark
+    app
+    color="dark"
+    dark
+    hide-on-scroll
+  >
+    <v-btn
+      href="/"
+      text
     >
-      <v-btn
-        href="/"
-        text
-      >
-        <v-icon>mdi-cog</v-icon>
-        <span class="mr-2 ml-2">Skill-Badge-Generator</span>
-      </v-btn>
-    </v-app-bar>
+      <v-icon>mdi-cog</v-icon>
+      <span class="mr-2 ml-2">Skill-Badge-Generator</span>
+    </v-btn>
+  </v-app-bar>
 </template>
