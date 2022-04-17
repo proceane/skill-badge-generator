5e2fd7f21ac4cefa83572b6b6471a6380205cdb3
proceane <proceane1@gmail.com>
2022-04-17 21:45:03 +0900

    add condition in PreviewArea

--- src/components/PreviewArea.vue
+++ src/components/PreviewArea.vue
@@ -3,49 +3,49 @@
     <v-card-title>Preview</v-card-title>
 
     <v-card-text>
-      <h3 class="mb-2" v-if="isShow">Language</h3>
+      <h3 class="mb-2" v-if="isShow && langSkillList != null">Language</h3>
       <div 
         v-html="langSkillList"
         v-if="langSkillList != null"
       ></div>
 
-      <br v-if="isShow">
-      <h3 class="mb-2" v-if="isShow">Front-End</h3>
+      <br v-if="isShow && frontSkillList != null">
+      <h3 class="mb-2" v-if="isShow && frontSkillList != null">Front-End</h3>
       <div 
         v-html="frontSkillList"
         v-if="frontSkillList != null"
       ></div>
 
-      <br v-if="isShow">
-      <h3 class="mb-2" v-if="isShow">Back-End</h3>
+      <br v-if="isShow && backSkillList != null">
+      <h3 class="mb-2" v-if="isShow && backSkillList != null">Back-End</h3>
       <div 
         v-html="backSkillList"
         v-if="backSkillList != null"
       ></div>
 
-      <br v-if="isShow">
-      <h3 class="mb-2" v-if="isShow">Mobile</h3>
+      <br v-if="isShow && mobileSkillList != null">
+      <h3 class="mb-2" v-if="isShow && mobileSkillList != null">Mobile</h3>
       <div 
         v-html="mobileSkillList"
         v-if="mobileSkillList != null"
       ></div>
 
-      <br v-if="isShow">
-      <h3 class="mb-2" v-if="isShow">Database</h3>
+      <br v-if="isShow && dbSkillList != null">
+      <h3 class="mb-2" v-if="isShow && dbSkillList != null">Database</h3>
       <div 
         v-html="dbSkillList"
         v-if="dbSkillList != null"
       ></div>
 
-      <br v-if="isShow">
-      <h3 class="mb-2" v-if="isShow">DevOps</h3>
+      <br v-if="isShow && devOpsSkillList != null">
+      <h3 class="mb-2" v-if="isShow && devOpsSkillList != null">DevOps</h3>
       <div 
         v-html="devOpsSkillList"
         v-if="devOpsSkillList != null"
       ></div>
 
-      <br v-if="isShow">
-      <h3 class="mb-2" v-if="isShow">Collaborate</h3>
+      <br v-if="isShow && collaboSkillList != null">
+      <h3 class="mb-2" v-if="isShow && collaboSkillList != null">Collaborate</h3>
       <div 
         v-html="collaboSkillList"
         v-if="collaboSkillList != null"  
